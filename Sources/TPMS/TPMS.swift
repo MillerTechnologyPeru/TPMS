d99a6364455d5451652ae8ffb7d0616edc0a146c
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-03-11 18:15:31 -0400

    Updayed `TirePressureSensor`

--- Sources/TPMS/TPMS.swift
+++ Sources/TPMS/TPMS.swift
@@ -5,9 +5,17 @@ import GATT
 /// Tire-pressure monitoring system sensor
 public struct TirePressureSensor: Equatable, Hashable, Codable, Sendable {
     
-    static var service: BluetoothUUID { .tpmsService }
+    public static var service: BluetoothUUID { .tpmsService }
+    
+    public static var companyIdentifier: CompanyIdentifier { .tomtomInternational }
     
     public let address: BluetoothAddress
+        
+    public let pressure: Pressure
+    
+    public let temperature: Temperature
+    
+    public let batteryLevel: BatteryLevel
     
     
 }
@@ -27,7 +35,10 @@ public extension TirePressureSensor {
     
     init?<T: AdvertisementData>(_ advertisement: T) {
         guard advertisement.serviceUUIDs == [Self.service],
-              let manufacturerData = advertisement.manufacturerData else {
+              let manufacturerData = advertisement.manufacturerData,
+              manufacturerData.companyIdentifier == Self.companyIdentifier,
+              manufacturerData.additionalData.count == 16,
+              let batteryLevel = BatteryLevel(rawValue: manufacturerData.additionalData[14]) else {
             return nil
         }
         self.address = BluetoothAddress(
@@ -43,5 +54,8 @@ public extension TirePressureSensor {
                     )
                 )
         )
+        self.pressure = Pressure(rawValue: UInt32(littleEndian: UInt32(bytes: (manufacturerData.additionalData[6], manufacturerData.additionalData[7], manufacturerData.additionalData[8], manufacturerData.additionalData[9]))))
+        self.temperature = Temperature(rawValue: UInt32(littleEndian: UInt32(bytes: (manufacturerData.additionalData[10], manufacturerData.additionalData[11], manufacturerData.additionalData[12], manufacturerData.additionalData[13]))))
+        self.batteryLevel = batteryLevel
     }
 }
