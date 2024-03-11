5fafc66c61dd8615d2ab83d9382f39e738777337
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-03-11 18:57:23 -0400

    Add `TPMSAdvertisementRow`

---
+++ TirePressure.swiftpm/Views/AdvertisementRow.swift
@@ -0,0 +1,71 @@
+//
+//  TPMSAdvertisementRow.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 4/12/23.
+//
+
+import Foundation
+import SwiftUI
+import Bluetooth
+import TPMS
+
+struct TPMSAdvertisementRow: View {
+    
+    @EnvironmentObject
+    private var store: AccessoryManager
+    
+    let advertisement: TirePressureSensor
+    
+    var body: some View {
+        StateView(
+            advertisement: advertisement
+        )
+    }
+}
+
+internal extension TPMSAdvertisementRow {
+    
+    struct StateView: View {
+        
+        let advertisement: TirePressureSensor
+                
+        var body: some View {
+            HStack {
+                // icon
+                VStack {
+                    Text(verbatim: advertisement.pressure.description)
+                }
+                .frame(width: 100)
+                
+                // Text
+                VStack(alignment: .leading) {
+                    Text(verbatim: advertisement.address.rawValue)
+                        .font(.title3)
+                    HStack {
+                        Text(verbatim: advertisement.temperature.description)
+                        Text(verbatim: advertisement.batteryLevel.description)
+                    }
+                    .foregroundColor(.gray)
+                    .font(.subheadline)
+                }
+            }
+            
+        }
+    }
+}
+
+// MARK: - Preview
+
+#if DEBUG
+struct TPMSAdvertisementRow_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        NavigationView {
+            List {
+                EmptyView()
+            }
+        }
+    }
+}
+#endif
