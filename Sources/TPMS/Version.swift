85ddec55a009f143792433b1f219aad343340b8f
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-03-11 16:52:35 -0400

    Add `TPMSVersion`

---
+++ Sources/TPMS/Version.swift
@@ -0,0 +1,15 @@
+//
+//  Version.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 3/11/24.
+//
+
+/// Tire-pressure monitoring system sensor version
+public enum TPMSVersion: UInt8 {
+    
+    case v1 = 0
+    case v2 = 1
+    case v3 = 2
+    case v4 = 3
+}
