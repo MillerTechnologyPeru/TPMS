5540a6d5ee38e602d20d7ab6a0a761afcb8c2b21
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-03-11 16:52:41 -0400

    Add `TirePressureSensor`

--- Sources/TPMS/TPMS.swift
+++ Sources/TPMS/TPMS.swift
@@ -2,3 +2,46 @@ import Foundation
 import Bluetooth
 import GATT
 
+/// Tire-pressure monitoring system sensor
+public struct TirePressureSensor: Equatable, Hashable, Codable, Sendable {
+    
+    static var service: BluetoothUUID { .tpmsService }
+    
+    public let address: BluetoothAddress
+    
+    
+}
+
+// MARK: - Identifiable
+
+extension TirePressureSensor: Identifiable {
+    
+    public var id: BluetoothAddress {
+        address
+    }
+}
+
+// MARK: - Advertisement
+
+public extension TirePressureSensor {
+    
+    init?<T: AdvertisementData>(_ advertisement: T) {
+        guard advertisement.serviceUUIDs == [Self.service],
+              let manufacturerData = advertisement.manufacturerData else {
+            return nil
+        }
+        self.address = BluetoothAddress(
+            bigEndian:
+                BluetoothAddress(
+                    bytes: (
+                        manufacturerData.additionalData[0],
+                        manufacturerData.additionalData[1],
+                        manufacturerData.additionalData[2],
+                        manufacturerData.additionalData[3],
+                        manufacturerData.additionalData[4],
+                        manufacturerData.additionalData[5]
+                    )
+                )
+        )
+    }
+}
