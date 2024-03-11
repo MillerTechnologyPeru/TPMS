1ef02e4b794ea68823d08d372c9a4c7c8b4985f8
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-03-11 18:57:08 -0400

    Add `TirePressureAppError`

---
+++ TirePressure.swiftpm/Model/Error.swift
@@ -0,0 +1,29 @@
+//
+//  Error.swift
+//
+//
+//  Created by Alsey Coleman Miller on 4/12/23.
+//
+
+import Foundation
+import Bluetooth
+import TPMS
+
+/// TPMS app errors.
+public enum TirePressureAppError: Error {
+    
+    /// Bluetooth is not available on this device.
+    case bluetoothUnavailable
+    
+    /// No service with UUID found.
+    case serviceNotFound(BluetoothUUID)
+    
+    /// No characteristic with UUID found.
+    case characteristicNotFound(BluetoothUUID)
+    
+    /// The characteristic's value could not be parsed. Invalid data.
+    case invalidCharacteristicValue(BluetoothUUID)
+    
+    /// Not a compatible peripheral
+    case incompatiblePeripheral
+}
